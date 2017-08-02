eb1dfe52539dd192cab5d6e787cfac80a7eece64
angel <angel@angel-Aspire-VN7-571G>
2017-08-03 00:43:05 +0800

    add index page using bootstrap template

---
+++ src/main/java/commconsistency/web/HomeController.java
@@ -0,0 +1,15 @@
+package commconsistency.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String home(Model model) {
+		return "index";
+	}
+	
+
+}
---
+++ src/main/java/commconsistency/web/WebConfig.java
@@ -0,0 +1,9 @@
+package commconsistency.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+}
