5fe7906871122cdae74e474b2f3d8f242f8c055c
angel <angel@angel-Aspire-VN7-571G>
2017-08-15 19:32:21 +0800

    update login.html

--- src/main/java/commconsistency/web/HomeController.java
+++ src/main/java/commconsistency/web/HomeController.java
@@ -1,14 +1,44 @@
 package commconsistency.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import commconsistency.dao.UserRepository;
+import commconsistency.domain.User;
 
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@RequestMapping("/")
 	public String home(Model model) {
 		return "index";
+	}
+	
+	@RequestMapping(value="/signin",method=RequestMethod.POST)
+	public String signin(@ModelAttribute User user,Model model) {
+		
+		List<String> roles = new ArrayList<String>();
+		roles.add("USER");
+		user.setRoles(roles);
+		userRepository.insert(user);
+		return "login";
+	}
+	
+	@RequestMapping(value="/signin",method=RequestMethod.GET)
+	public String signin(Model model) {
+		User user = new User();
+		model.addAttribute("user",user);
+		return "signin";
 	}
 }
--- src/main/java/commconsistency/web/WebConfig.java
+++ src/main/java/commconsistency/web/WebConfig.java
@@ -10,7 +10,6 @@ public class WebConfig extends WebMvcConfigurerAdapter{
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/login").setViewName("login");
-		registry.addViewController("/signin").setViewName("signin");
 	}
 
 }
